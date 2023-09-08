574b5b52d41a2731b7cf99274850fa2a5593e50d
Vincent Kerob <vkerob@1D1.42angouleme.fr>
2023-09-08 11:52:05 +0200

    maj

--- CPP_0/ex01/PhoneBook.cpp
+++ CPP_0/ex01/PhoneBook.cpp
@@ -10,12 +10,13 @@ PhoneBook::~PhoneBook(void) {
 	return ;
 }
 
-void	PhoneBook::add_contact(std::string first_name, std::string last_name, std::string nickname, std::string phone_number, std::string darkest_secret, int i)
+int	PhoneBook::add_contact(std::string first_name, std::string last_name, std::string nickname, std::string phone_number, std::string darkest_secret, int i)
 {
+	if (contact[i].set_phone_number(phone_number))
+		return (1);
 	contact[i].set_string(first_name, "first_name");
 	contact[i].set_string(last_name, "last_name");
 	contact[i].set_string(nickname, "nickname");
-	contact[i].set_phone_number(phone_number);
 	contact[i].set_string(darkest_secret, "darkest_secret");
 
 
--- CPP_0/ex01/main.cpp
+++ CPP_0/ex01/main.cpp
@@ -2,18 +2,33 @@
 #include"PhoneBook.hpp"
 #include<cstdlib>
 
-void	stack_input_and_check_empty(std::string &str)
+int	stack_input_and_check_empty(std::string &str, std::string entry)
 {
-	std::cin >> str;
+	std::cout << entry;
+	std::getline(std::cin, str);
 	if (str.empty())
-		exit(0);
+		return (1);
+	if (str == "\n")
+		stack_input_and_check_empty(str, entry);
+	return (0);
 }
 
-void	stack_all_info(PhoneBook &phonebook, int i)
+int	stack_all_info_and_add_contact(PhoneBook &phonebook, int &i)
 {
 	std::string	first_name, last_name, nickname, phone_number, darkest_secret;
-
+	if (stack_input_and_check_empty(first_name, "first name: "))
+		return (1);
+	if (stack_input_and_check_empty(last_name, "last name: "))
+		return (1);
+	if (stack_input_and_check_empty(nickname, "nickname: "))
+		return (1);
+	if (stack_input_and_check_empty(phone_number, "phone number: "))
+		return (1);
+	if (stack_input_and_check_empty(darkest_secret, "darknest secret: "))
+		return (1);
+	i++;
 	phonebook.add_contact(first_name, last_name, nickname, phone_number, darkest_secret, i);
+	return (0);
 }
 
 int	main(void)
@@ -25,14 +40,19 @@ int	main(void)
 	i = 0;
 	while(1)
 	{
-		std::cout << "PhoneBook : ";
-		stack_input_and_check_empty(input);
+		i %= 8;
+		if (stack_input_and_check_empty(input, "PhoneBook : "))
+			return (0);
 		if (input == "EXIT")
 			return(0);
-		//else if (input == "SEARCH")
+		else if (input == "SEARCH")
+		{
+			
+		}
 		else if (input == "ADD")
 		{
-			stack_all_info(phonebook, i);
+			if (stack_all_info_and_add_contact(phonebook, i))
+				return (0);
 		}
 		else
 		{
