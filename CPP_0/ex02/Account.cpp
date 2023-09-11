896ee4cbf704ccdbbfa4ff14767ee61479b45066
Vincent Kerob <vkerob@1D4.42angouleme.fr>
2023-09-11 17:51:32 +0200

    maj

--- CPP_0/ex02/Account.cpp
+++ CPP_0/ex02/Account.cpp
@@ -8,6 +8,10 @@ int	Account::_totalAmount = 0;
 
 Account::Account( int initial_deposit )
 {
+	_accountIndex = _nbAccounts;
+	_nbAccounts++;
+	_amount = initial_deposit;
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
 	this->_amount = initial_deposit;
 	return ;
 }
@@ -42,6 +46,7 @@ void	Account::displayAccountsInfos( void )
 	std::cout << "accounts:" << _nbAccounts << ";total:"
 	<< _totalAmount << ";deposits:" << _totalNbDeposits
 	<< ";withdrawals:" << _totalNbWithdrawals << std::endl;
+
 }
 
 void	Account::makeDeposit( int deposit )
@@ -49,23 +54,31 @@ void	Account::makeDeposit( int deposit )
 	this->_totalNbDeposits += 1;
 	this->_totalAmount += deposit;
 	this->_nbDeposits += 1;
+	std::cout << "index:" << this->_accountIndex  << ";p_amount:" << this->_amount;
 	this->_amount += deposit;
+	std::cout << ";deposit:" << deposit << ";amount:" << this->_amount << ";nb_deposits:" << this->_nbDeposits << std::endl;
 }
 
 bool	Account::makeWithdrawal( int withdrawal )
 {
+	if (this->_totalAmount -= withdrawal < 0)
+		return (1);
 	this->_totalNbWithdrawals += 1;
 	this->_totalAmount -= withdrawal;
 	this->_nbWithdrawals += 1;
+	std::cout << "index:" << this->_accountIndex  << ";p_amount:" << this->_amount;
 	this->_amount -= withdrawal;
+	std::cout << ";withdrawal:" << withdrawal << ";amount:" << this->_amount << ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+	return (0);
 }
 
 int		Account::checkAmount( void ) const
 {
-	
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << std::endl;
+	return (1);
 }
 
 void	Account::displayStatus( void ) const
 {
-	
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposits:" <<_nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
 }
