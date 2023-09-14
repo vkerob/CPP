8e9cfb269fc6d3810982ac1ddef6f9393ae5e1e3
neweth <vincepub45@gmail.com>
2023-09-14 02:48:53 +0200

    maj

--- CPP_0/ex01/PhoneBook.hpp
+++ CPP_0/ex01/PhoneBook.hpp
@@ -10,8 +10,8 @@ public:
 	PhoneBook(void);
 	~PhoneBook(void);
 	int			add_contact(std::string first_name, std::string last_name, std::string nickname, std::string phone_number, std::string darkest_secret, int &i);
-	void		print_info(PhoneBook &phonebook) const;
-	int			get_index(PhoneBook &phonebook) const;
+	void		print_info(PhoneBook &phonebook);
+	int			get_index(PhoneBook &phonebook);
 
 private:
 	std::string fill_string(PhoneBook &phonebook, std::string identifier, int i);
